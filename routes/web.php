8bff63b7cb1f057c0abb1c2daf0c3c504edc029c
User <User@AXELLE-PC>
2023-04-21 13:01:30 +0100

    Laravel Project With Bootstrap and CSS and Authentication configuration

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/shop', function () {
+    return view('Shop');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
